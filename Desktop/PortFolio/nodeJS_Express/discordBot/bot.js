e367dc16dd25b64f0b08044ded6463c7528db5d5
vardan.davtyan.y <vardan.davtyan.y@tumo.org>
2020-09-11 01:29:09 +0500

    Update

--- Desktop/PortFolio/nodeJS_Express/discordBot/bot.js
+++ Desktop/PortFolio/nodeJS_Express/discordBot/bot.js
@@ -6,8 +6,8 @@ const client = new Discord.Client()
 client.on('ready', () => {
    console.log(`Connected as\` ${client.user.tag} `)
 
-   var array = ['Google', 'Youtube', 'Twitch', 'StandOff', 'E3']
-   var randomThing = array[Math.floor(Math.random() * array.length-1)]
+   var array = ['Google', 'Youtube', 'Twitch', 'StandOff', 'E3', 'Netflix']
+   var randomThing = array[Math.floor(Math.random() * array.length)]
 
    client.user.setActivity(`${randomThing}`, { type: 'WATCHING' })
 
@@ -19,6 +19,7 @@ client.on('ready', () => {
       })
 
       let botChannel = client.channels.cache.get('752890484581466212')
+      botChannel.send('I\'m Connected')
    })
 })
 
@@ -28,12 +29,24 @@ client.on('message', msg => {
       return
    }
 
-   if ((msg.content.includes('Hello') || msg.content.includes('Hi')) && msg.content.includes('Mikle')) {
-      msg.reply(`Hello`)
+   if ((msg.content.includes('Hello') || msg.content.includes('Hi')) && (msg.content.includes('Mikle') || msg.content.includes('Man'))) {
+      msg.reply(`Hello, Whats Up`)
+   }
+
+   if ((msg.content.includes('Whats Up') || msg.content.includes('whats up')) && (msg.content.includes('Mikle') || msg.content.includes('Man'))) {
+      msg.reply(`Nothing`)
+   }
+
+   if (msg.content.toLowerCase() == "please help"){
+      msg.reply(`Type '$help'`)
    }
 
    if ((msg.content.toLowerCase().includes('fuck you mikle'))) {
       msg.reply(':(')
+   }
+
+   if (msg.content == '$mikle'){
+      msg.channel.send(`tag: ${client.user.tag},\nuser name: ${client.user.username},\nid: ${client.user.id}.`)
    }
 
    if (msg.content.startsWith('$')) {
@@ -57,8 +70,10 @@ function helpCommand(arguments, receivedMessage) {
    if (arguments.length == 0) {
 
       receivedMessage.channel.send(`Commands: 
-                                    $help channels.all - showing all Channels
-                                    $help server.name`)
+      $help channels.all - showing all Channels
+      $help server.name - showing Servers Name
+      $help server.id - showing Servers ID
+      $help myID - showing Your Account ID`)
 
    } else {
       if (arguments == "channels.all") {
@@ -72,9 +87,23 @@ function helpCommand(arguments, receivedMessage) {
          })
       }
 
+      //Commands`
+
       if (arguments == "server.name"){
          client.guilds.cache.forEach((guild) => {
             receivedMessage.channel.send(`NAME: ${guild.name}`)
+         })
+      }
+
+      if (arguments == "server.id"){
+         client.guilds.cache.forEach((guild) => {
+            receivedMessage.channel.send(`ID: ${guild.id}`)
+         })
+      }
+
+      if (arguments == "myID"){
+         client.guilds.cache.forEach(() => {
+            receivedMessage.reply(`Your ID: ${receivedMessage.author.id.toString()}`)
          })
       }
    }
